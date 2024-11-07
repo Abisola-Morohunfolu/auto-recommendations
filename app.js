49f4c95b37994c6c69fe63404645919d504f7598
Bisola <bisola@equally.ai>
2024-11-08 00:50:18 +0100

    added db connection

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const { connectToDatabase, endDatabaseConnection } = require("./db/connect");
 const app = express();
 
 const port = process.env.PORT || 8000;
 
 const start = async () => {
   try {
+    await connectToDatabase();
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
+    endDatabaseConnection();
   } catch (error) {
     console.log(error);
   }
